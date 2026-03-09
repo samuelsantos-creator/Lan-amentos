af5fdfc61f9803d839da7096c8a51788d4f94997
samuelsantos-creator <samuel.santos@progeral.com.br>
2026-03-09 13:14:34 -0300

    V1.2.1

--- sw.js
+++ sw.js
@@ -4,8 +4,7 @@ const ASSETS = [
   './index.html',
   './manifest.json',
   './logo progeral azul.png',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icons/app-icon.jpg',
   'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;600;700&family=IBM+Plex+Mono:wght@400;600&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
